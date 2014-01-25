d8a6c13d980f9239ee5c785325cca912e1753c92
Ross Knudsen <ross@gravitycomputing.co.nz>
2014-01-25 15:08:25 +1300

    Initial commit of OpenMCDF source.  Added strong naming to projects and upgraded to VS2012 compliant projects.  Fixed test reference and commented out broken code in tests.

---
+++ OLECompoundFileStorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenMcdf")]
+[assembly: AssemblyDescription("MS Compound File Storage .NET Implementation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Federico Blaseotto")]
+[assembly: AssemblyProduct("OpenMcdf")]
+[assembly: AssemblyCopyright("Copyright © 2010-2011, Federico Blaseotto")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffc13791-ddf0-4d14-bd64-575aba119190")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5.4.*")]
+
+[assembly: InternalsVisibleTo("OpenMcdfTest, PublicKey=" +
+    "002400000480000094000000060200000024000052534131000400000100010047aea32fe749a1" +
+    "b06c5a6b06315303d1f1bedbe7f019cacef2b5b847b739a02b031b0b0892751461f09501005314" +
+    "4d281191b95ab8aa5021a11b4c6623872de4fa490979a7b10f4dce8a055ae5ca2200f92c2a21c3" +
+    "8ca9435c66169caeb36d9dd139f94d7350feb050d9fc46fda5093a20e2737397ef05c924772ff6" +
+    "1683c9c1")]
